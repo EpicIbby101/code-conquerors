f8ac8a78470781c1e073c0a2bc4d6da84a3ea284
EpicIbby101 <kevin_grittner@outlook.com>
2023-11-02 19:50:18 +0000

    Finalised setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-
 function App() {
   return (
     <div className="App">
       <div className='text-2xl font-bold bg-gradient-to-r from-red-400 via-yellow-500 to-green-400'>
-        Testing Tailwind Styles
+        Testing Tailwind Styles 😎
       </div>
     </div>
   );
