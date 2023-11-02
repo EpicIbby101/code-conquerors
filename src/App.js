03f87d56e3c797217b2d804327558f6bdc2bac65
EpicIbby101 <kevin_grittner@outlook.com>
2023-11-02 19:42:39 +0000

    Installed barebones React App with essential modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='text-2xl font-bold bg-gradient-to-r from-red-400 via-yellow-500 to-green-400'>
+        Testing Tailwind Styles
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{jsx,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["inter", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
