daa1e9de4a1fd9b0ec2cda12525de829ea9a48ae
Craig G <gibson.craig05@gmail.com>
2023-11-06 22:52:45 +0000

    further edits to the styling, added text and button colours

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
     return (
         <div heroSection>
             <div class="container mx-auto px-4 py-10 bg-black flex items-center justify-between">
-                <div className="text-left font-bold text-xl px-20 py-40 text-white rounded-md">
+                <div className="text-left font-bold text-xl px-20 py-40 text-rose-300 rounded-md">
                     <h1>Code Conquerors</h1>
 
                     <div className="text-left font-bold text-5xl py-10 text-white rounded-md">
@@ -13,18 +13,18 @@ const HeroSection = () => {
 
                     <button
                         type="button"
-                        className="focus:outline-none text-white bg-purple-500 hover:bg-purple-600 font-medium rounded-lg text-sm px-5 py-2.5 mx-2.5">
+                        className="focus:outline-none text-black bg-rose-300 hover:bg-purple-600 font-medium rounded-lg text-sm px-5 py-2.5">
                         Search for books
                     </button>
 
                     <button
                         type="button"
-                        className="focus:outline-none text-white bg-purple-500 hover:bg-purple-600 font-medium rounded-lg text-sm px-5 py-2.5 mx-2.5">
+                        className="focus:outline-none text-black bg-amber-200 hover:bg-purple-600 font-medium rounded-lg text-sm px-5 py-2.5 mx-2.5">
                         Search for videos
                     </button>
                 </div>
                 <div className="">
-                    <img src={""} alt="Many lines of colourful code"></img>
+                    <img src="/images/bgimage.jpg" alt="Many lines of colourful code"></img>
                 </div>
             </div>
         </div>
