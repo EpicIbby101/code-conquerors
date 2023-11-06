cd5e0937b945d5a54eb69459361d386c12109459
Craig G <gibson.craig05@gmail.com>
2023-11-06 21:21:39 +0000

    setting up hero section component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+// import bg from "./images/bgimage.jpg"
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
+        <HeroSection />
       </div>
     </Router>
   );
 }
+
+
 
 export default App;
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <div heroSection className="font-bold px-6 py-3 text-black rounded-md">
+            <h1>Code Conquerors</h1>
+        </div>
+    )
+}
+
+
+
+// class HeroStyle extends React.Component {
+//     render() {
+//       return (
+//         <div>
+//         <h1 style={{color: "red"}}></h1>
+//         </div>
+//       );
+//     }
+//   }
+
+
+
+
+export default HeroSection
