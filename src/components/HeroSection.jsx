f3943f8680f7aa44d5b87dc377c3256c882011ba
Craig G <gibson.craig05@gmail.com>
2023-11-07 21:03:03 +0000

    fixed image not displaying correctly. Added colour gradient to the buttons

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import heroImage from "../images/heroImage.jpg"
 
 const HeroSection = () => {
     return (
         <div heroSection>
-            <div class="container mx-auto px-4 py-10 bg-black flex items-center justify-between">
+            <div class="container mx-auto px-4 py-10 bg-neutral-800 flex items-center justify-between">
                 <div className="text-left font-bold text-xl px-20 py-40 text-rose-300 rounded-md">
                     <h1>Code Conquerors</h1>
 
@@ -13,18 +14,18 @@ const HeroSection = () => {
 
                     <button
                         type="button"
-                        className="focus:outline-none text-black bg-rose-300 hover:bg-purple-600 font-medium rounded-lg text-sm px-5 py-2.5">
+                        className="focus:outline-none text-black bg-gradient-to-r from-amber-200 to-rose-300 hover:bg-purple-600 font-medium rounded-lg text-sm px-5 py-2.5">
                         Search for books
                     </button>
 
                     <button
                         type="button"
-                        className="focus:outline-none text-black bg-amber-200 hover:bg-purple-600 font-medium rounded-lg text-sm px-5 py-2.5 mx-2.5">
+                        className="focus:outline-none text-black bg-gradient-to-r from-rose-300 to-amber-200 hover:bg-purple-600 font-medium rounded-lg text-sm px-5 py-2.5 mx-2.5">
                         Search for videos
                     </button>
                 </div>
-                <div className="">
-                    <img src="/images/bgimage.jpg" alt="Many lines of colourful code"></img>
+                <div className="h-auto max-w-full rounded-lg .transition-shadow">
+                    <img src={heroImage} alt="Many lines of colourful code"></img>
                 </div>
             </div>
         </div>
