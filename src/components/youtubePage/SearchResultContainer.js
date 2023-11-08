403574bed6078956a37d57a986ac73c18c72a2be
saycatnab <saysaygood83@gmail.com>
2023-11-08 19:25:07 +0000

    added in the searchResultContainer as a class which extends to Component

--- src/components/youtubePage/SearchResultContainer.js
+++ src/components/youtubePage/SearchResultContainer.js
@@ -3,3 +3,13 @@ import SearchForm from "./SearchForm";
 import API from "../youtubePage/YoutubeAPI";
 
 let baseURL = "https://www.youtube.com/embed/";
+
+class SearchResultContainer extends Component {
+    state = {
+        search: "",
+        results: []
+      };
+
+    }
+
+export default SearchResultContainer;
