b9d4dd8c49f17fdbc6f449bf1a1fef5fb410b547
blackclover221 <yasminismail33@gmail.com>
2023-11-08 13:23:34 +0000

    added the initial 10 questions to the data.js for the quiz targeting html and css

---
+++ src/data.js
@@ -0,0 +1,84 @@
+const data = [
+    {
+        question: "1. What does HTML stand for?",
+        incorrectAnswers: [
+            "b. High-level Text Management Language",
+            "c. Hyperlink and Text Management Language",
+        ],
+        correctAnswer: "a. Hyper Text Markup Language",
+    },
+
+    {
+        question: "2. Which HTML tag is used for creating a hyperlink?",
+        incorrectAnswers: [
+            "a. <link>",
+            "c. <hyperlink>",
+        ],
+        correctAnswer: "b. <a>",
+    },
+
+    {
+        question: "3. What is the purpose of the HTML <div> element?",
+        incorrectAnswers: [
+            "a. It defines a paragraph of text.",
+            "c. It defines a clickable link.",
+        ],
+        correctAnswer: "b. It defines a division or a section in an HTML document.",
+    },
+
+    {
+        question: "4. Which HTML element is used to specify the character encoding for an HTML document?",
+        incorrectAnswers: ["<script>", "<headStyle>", "<css>"],
+        correctAnswer: "<style>",
+    },
+
+    {
+        question: "5. What is the correct HTML element for adding a line break?",
+        incorrectAnswers: [
+            "<break>",
+            "<lb>",
+        ],
+        correctAnswer: "<br>",
+    },
+
+    {
+        question: "6. In CSS, what property is used to change the background color of an element?",
+        incorrectAnswers: [
+            "a. color",
+            "c. bgcolor",
+        ],
+        correctAnswer: "b. background-color",
+    },
+
+    {
+        question: "7. What is the CSS box model used for?",
+        incorrectAnswers: [
+            "a. To define the structure of a webpage.",
+            "c. To manage JavaScript functions.",
+        ],
+        correctAnswer: "b. To specify the layout and design of an element.",
+    },
+
+    {
+        question: "8. Which CSS property is used to control the font size of text?",
+        incorrectAnswers: ["font-size", "text-size", "size"],
+        correctAnswer: "a. font-size",
+    },
+
+    {
+        question: "9. How do you center an element horizontally in CSS?",
+        incorrectAnswers: [
+            "a. text-align: center;",
+            "c. center: true;",
+        ],
+        correctAnswer: "b. margin-left: auto; margin-right: auto;",
+    },
+
+    {
+        question: "10. What is the purpose of the CSS property 'float'?",
+        incorrectAnswers: [
+            "a. To make text bold.",
+            "c. To add interactive buttons.",
+        ],
+        correctAnswer: "b. To allow text to wrap around an element.",
+    },
