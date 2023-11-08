0390ec7ac06b5dd83f732fff831c89b083b49211
Craig G <gibson.craig05@gmail.com>
2023-11-08 15:41:43 +0000

    working on footer styling (changed color and made it full width)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import logo from '../assets/'
+import Noimage from '../assets/Noimage.png'
 
 const Footer = () => {
     return (
-        <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+        <footer className="bg-neutral-800 rounded-lg shadow dark:bg-gray-900">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0">
-                        <img src="" className="h-8 mr-3" alt="Flowbite Logo" />
+                    <a className="flex items-center mb-4 sm:mb-0">
+                        <img src={Noimage} className="h-8 mr-3" alt="Code Conquerors Logo" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
