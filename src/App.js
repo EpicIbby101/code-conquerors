e8d5410b3387b5e6f55b71740245ea95b4675e19
EpicIbby101 <kevin_grittner@outlook.com>
2023-11-08 19:05:58 +0000

    Fixed some imports

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import BookReading from "./components/BookReading";
 import Modal from "react-modal";
-
-
-const rootElement = document.getElementById('root');
-Modal.setAppElement(rootElement);
-
 import HeroSection from "./components/HeroSection";
-import BookReading from "./components/BookReading";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
+
+const rootElement = document.getElementById("root");
+Modal.setAppElement(rootElement);
 
 function App() {
   return (
@@ -25,7 +22,5 @@ function App() {
     </Router>
   );
 }
-
-
 
 export default App;
