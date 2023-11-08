6c38c84c31d8059bb880404d46981a9289df59a1
blackclover221 <yasminismail33@gmail.com>
2023-11-08 16:01:27 +0000

    imported the different scripts into into index.js and added logic for correct and wrong answers

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/tailwind.css';
+import "./index.css";
+import Quiz from "./components/quiz"
+import { QuizProvider } from "./contexts/quiz";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <QuizProvider>
+      <Quiz />
+    </QuizProvider>
   </React.StrictMode>
 );
 
