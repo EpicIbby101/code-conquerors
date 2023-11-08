b04a22d31ee7fc537aa52df9d92f196b04242919
saycatnab <saysaygood83@gmail.com>
2023-11-08 21:51:03 +0000

    Small fixes on api

--- src/components/youtubePage/YoutubeAPI.js
+++ src/components/youtubePage/YoutubeAPI.js
@@ -2,11 +2,14 @@ import axios from "axios";
 
 // BASEURL for the api to get data with added tags for the most ideal youtube videos displayed (orderViewCount, Maxresults, videoDuration)
 // APIKEY > includes added words to the q= and api key after
+
 const BASEURL = "https://youtube.googleapis.com/youtube/v3/search?maxResults=10&order=viewCount&videoDefinition=high&type=video&videoDuration=medium&part=snippet&q=";
 const APIKEY = "%20crash%20course%20coding&key=AIzaSyBNnaKYGPDH70xUq3QNi_JqgqgjjIOnvKE";
+// "%20crash%20course%20coding&key=AIzaSyBNnaKYGPDH70xUq3QNi_JqgqgjjIOnvKE";
 
 // Function to fetch the data from the API 
 export default {
+
   search: function(query) {
     return axios.get(BASEURL + query + APIKEY
       , console.log(BASEURL + query + APIKEY)
