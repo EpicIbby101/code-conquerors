6e7ca09f1e8b4d0b796f813cabe8200c17a523e0
saycatnab <saysaygood83@gmail.com>
2023-11-08 18:26:39 +0000

    Added in the p tag below the h2 tag with more description of the page

--- src/components/youtubePage/SearchForm.js
+++ src/components/youtubePage/SearchForm.js
@@ -7,6 +7,12 @@ function SearchForm(props) {
       <div className="lg:flex lg:flex-col space-y-4 bg-black">
         <div>
             <h2 className=" text-center text-4xl md:text-4xl sm:text-4xl font-bold mb-3 text-amber-200 mt-32">Youtube Video Search</h2>
+            <br>
+            </br>
+            <p className="text-md text-center md:text-lg sm:text-lg mb-4 text-white">
+                We have a large collection of suitable programming crash courses/tutorials to help you
+                get the most out of your studies as well. 
+            </p>
         </div>    
     </div>
   )
