a556f29032ded99480c75941274d0f5d3da74652
saycatnab <saysaygood83@gmail.com>
2023-11-08 18:56:14 +0000

    Made a variable for the API key containing the youtube api

--- src/components/youtubePage/YoutubeAPI.js
+++ src/components/youtubePage/YoutubeAPI.js
@@ -1,3 +1,4 @@
 import axios from "axios";
 
 const BASEURL = "https://youtube.googleapis.com/youtube/v3/search?maxResults=10&order=viewCount&videoDefinition=high&type=video&videoDuration=medium&part=snippet&q=";
+const APIKEY = "%20crash%20course%20coding&key=AIzaSyBNnaKYGPDH70xUq3QNi_JqgqgjjIOnvKE";
