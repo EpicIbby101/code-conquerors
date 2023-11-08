ed486bde5611cf64267e22587723ffe5dbc9c7bb
saycatnab <saysaygood83@gmail.com>
2023-11-08 18:54:06 +0000

    Made a const variable for the base url

--- src/components/youtubePage/YoutubeAPI.js
+++ src/components/youtubePage/YoutubeAPI.js
@@ -1,1 +1,3 @@
 import axios from "axios";
+
+const BASEURL = "https://youtube.googleapis.com/youtube/v3/search?maxResults=10&order=viewCount&videoDefinition=high&type=video&videoDuration=medium&part=snippet&q=";
