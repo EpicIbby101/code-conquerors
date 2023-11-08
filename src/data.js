76782410feaf5918f0e4cb69d180cdef34207fc7
blackclover221 <yasminismail33@gmail.com>
2023-11-08 15:08:30 +0000

    added 10 more questions targeting js, node and react to the data script

--- src/data.js
+++ src/data.js
@@ -82,3 +82,97 @@ const data = [
         ],
         correctAnswer: "b. To allow text to wrap around an element.",
     },
+    {
+        question: "11. What is the result of 5 + '3' in JavaScript?",
+        incorrectAnswers: [
+            "a. 8",
+            "c. 53",
+        ],
+        correctAnswer: "b. '53'",
+    },
+
+    {
+        question: "12. What is the purpose of the JavaScript function 'parseInt()'?",
+        incorrectAnswers: [
+            "a. To display a pop-up dialog.",
+            "c. To concatenate two strings.",
+        ],
+        correctAnswer: "b. To parse a string and return an integer.",
+    },
+
+    {
+        question: "13. What is the scope of a variable declared with the 'var' keyword in JavaScript?",
+        incorrectAnswers: [
+            "a. Local",
+            "c. Both global and local",
+        ],
+        correctAnswer: "b. Global",
+    },
+
+    {
+        question: "14. Which operator is used for strict equality in JavaScript?",
+        incorrectAnswers: [
+            "a. ==",
+            "c. =",
+        ],
+        correctAnswer: "b. ===",
+    },
+
+    {
+        question: "15. What is the purpose of the JavaScript 'typeof' operator?",
+        incorrectAnswers: [
+            "a. To check if a variable is defined.",
+            "c. To assign a value to a variable.",
+        ],
+        correctAnswer: "b. To determine the data type of a value.",
+    },
+
+    {
+        question: "16. What is React?",
+        incorrectAnswers: [
+            "a. A server-side rendering framework.",
+            "c. A database management system.",
+        ],
+        correctAnswer: "b. A JavaScript library for building user interfaces.",
+    },
+
+    {
+        question: "17. In React, what is a component?",
+        incorrectAnswers: [
+            "a. A programming language.",
+            "c. A network protocol.",
+        ],
+        correctAnswer: "b. A unit of user interface.",
+    },
+
+    {
+        question: "18. What is JSX in React?",
+        incorrectAnswers: [
+            "a. JavaScript Extra",
+            "c. JavaScript Extension",
+        ],
+        correctAnswer: "b. JavaScript XML",
+    },
+
+    {
+        question: "19. How do you update the state of a React component?",
+        incorrectAnswers: [
+            "a. By directly modifying the state object.",
+            "c. By using the 'render' method.",
+        ],
+        correctAnswer: "b. Using the 'setState' method.",
+    },
+
+    {
+        question: "20. What is React Router used for?",
+        incorrectAnswers: [
+            "a. To fetch data from APIs.",
+            "c. To create forms in React.",
+        ],
+        correctAnswer: "b. To manage navigation and routing in a React application.",
+    },
+
+];
+
+export default data;
+
